8d7440a5a0ec44a81f60b238870700d5e294f3d7
free.dev <tima.75.com@gmail.com>
2020-02-21 19:43:44 +0600

    fix

--- cmd/root.go
+++ cmd/root.go
@@ -9,5 +9,15 @@ import (
 )
 //Execute connect to db
 func Execute() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+ "password=%s dbname=%s sslmode=disable", pg.host, pg.port, pg.ser, pg.password, pg.dbname)
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+  	if err != nil {
+    panic(err)
+  	}
+  	defer db.Close()
+  	err = db.Ping()
+  	if err != nil {
+    	panic(err)
+  	}
+  	fmt.Println("Successfully connected!")
 }
